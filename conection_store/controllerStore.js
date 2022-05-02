3e982e1742be0fde9bc65b4d77c2c598b49d1a2a
Glitch-loop <renetdejesus@gmail.com>
2022-05-02 10:26:37 -0500

    Corrigiendo la conexción a base de datos

--- conection_store/controllerStore.js
+++ conection_store/controllerStore.js
@@ -1,32 +1,70 @@
-const connection = require('./connectionStore.js');
+const mysql = require('mysql');
+const config = require('../config.js');
 
-function query(query){
-    return new Promise((resolve, reject) => {
-        connection.query(`${query}`, (err, result) => {
-            if(err){
-                // console.log(`[ERROR] ${err}`); 
-                let error = 0
-                if(err.errno == 1452)
-                    error = "Se esta intentando asingar a una llave foraena que no existe"
-                    
-                if((err.errno == 1054) || (err.errno == 1064))
-                    error = "Se paso como parametro un dato diferente al permitido"
-                
-                if(err.errno == 1366)
-                    error = "Se esta intentando ingresar un dato no valido para un campo"
 
-                if(err.errno == 1292)
-                    error = "Formato invalido, error 1292"
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
+}
 
-                if(err.errno == 1062)
-                    error = err.sqlMessage
-               
-                return reject(error);
-            }else {
-                return resolve(result);
-            }  
+let connection;
+
+async function handleCon(){
+    connection = mysql.createConnection(dbconfig); //Creamos conexción
+    connection.connect((err)=>{
+        if(err){
+            console.error('[db err]', err);
+            setTimeout(handleCon(), 2000); //En caso de que haya problemas se manda a llamar de nuevo
+        }else{
+        }
+    connection.on('error', err => {
+            console.log('[db err]', err);
+            if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+                handleCon(); //Manejamos el error de conexción perdida
+            }else{
+                throw error; //En caso de no solucionarse lanzamos un error
+            }
         })
     })
+}
+
+
+async function  query(query){
+    return await handleCon()
+    .then(() => {
+        return new Promise((resolve, reject) => {
+            connection.query(`${query}`, (err, result) => {
+                if(err){
+                    let error = 0
+                    if(err.errno == 1452)
+                        error = "Se esta intentando asingar a una llave foraena que no existe"
+                        
+                    if((err.errno == 1054) || (err.errno == 1064))
+                        error = "Se paso como parametro un dato diferente al permitido"
+                    
+                    if(err.errno == 1366)
+                        error = "Se esta intentando ingresar un dato no valido para un campo"
+    
+                    if(err.errno == 1292)
+                        error = "Formato invalido, error 1292"
+    
+                    if(err.errno == 1062)
+                        error = err.sqlMessage
+                    connection.end()               
+                    return reject(console.log("conexion cerrada")); //Todo este proceso es para manejar errores de base de datos
+                }else {
+                    connection.end();
+                    return resolve(result); //Retornamos el resultado obtenido
+                }  
+            })
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 }
 
 module.exports = {
